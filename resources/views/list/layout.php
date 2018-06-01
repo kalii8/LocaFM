5a014ca6c7e91189d8ccdb11d5cb2981e5d977bb
Enrique Calvo <encalram@gmail.com>
2018-06-01 12:07:10 +0200

    list controler and layout

---
+++ resources/views/list/layout.php
@@ -0,0 +1,35 @@
+<?php foreach($song as $song) : ?>
+
+<div class="songs">
+    <!-- <div class="image">
+        <img src="<?= $song['image_url'] ?>" />
+    </div> -->
+    <div class="info">
+        <h2 class="name"><?= $song['name'] ?></h2>
+        <div class="code"><?= $song['code'] ?></div>
+        <div class="author">
+        <?= $song['author'] ?>
+        <a href="<?php $site_url ?>http://www.cbp-exercises.test/week6/day30/LocaFM/public/?page=form&id=<?=$song['id']?>">Edit</a>
+        <a href="<?php $site_url ?>http://www.cbp-exercises.test/week6/day30/LocaFM/public/?page=delete&id=<?=$song['id']?>">Edit</a>
+        </div>
+    </div>
+</div>
+
+<?php endforeach ?>
+
+<style>
+.songs{
+    display:flex;
+    padding: 5px;
+}
+.info{
+    padding: 10px;
+}
+.name {
+        text-align: center;
+    }
+img{
+    height: 200px;
+    width: 130px;
+}
+</style>
