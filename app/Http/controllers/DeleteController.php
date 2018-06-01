4dd809df53ade496974ce85631420a27e9a5a832
Nicole Blazier <nicole.blazier@gmail.com>
2018-06-01 12:53:57 +0200

    Add Delete Controller

---
+++ app/Http/controllers/DeleteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\Http\controllers;
+
+use \kalii8\MVC\db\db;
+use app\Song;
+
+class DeleteController
+{
+    public function index()
+    {
+        if (!isset($_GET['id'])) {
+            echo 'Error';
+        } else {
+            $song = Song::find($_GET['id']);
+        }
+
+        $song->delete();
+    
+        include __DIR__ . '/../../../resources/views/wrapper.php'; 
+    }
+    
+}
